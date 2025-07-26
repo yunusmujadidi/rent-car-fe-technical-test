3a08d45131ae7aaca4a3eb550168f04bcf8eb05c
yunusmujadidi <yunusmujadidi@gmail.com>
2025-07-26 20:25:22 +0700

    feat: add orders page and data table

--- src/app/(dashboard)/(routes)/cars/page.tsx
+++ src/app/(dashboard)/(routes)/cars/page.tsx
@@ -1,8 +1,8 @@
+import { PlusCircle, Search } from "lucide-react";
+
 import { CarCard } from "@/components/main/car-card";
 import { Button } from "@/components/ui/button";
-
 import { Input } from "@/components/ui/input";
-import { PlusCircle, Search } from "lucide-react";
 
 const CarsPage = () => {
   return (
@@ -23,10 +23,10 @@ const CarsPage = () => {
       {/* search bar */}
       <div className="relative">
         <Input
-          className="max-w-3xl pl-10"
+          className="max-w-3xl pl-10 bg-foreground"
           placeholder="Search car by name or ID"
         />
-        <Search className="w-5 h-5 text-muted-foreground absolute left-3 top-1/2 transform -translate-y-1/2" />
+        <Search className="w-5 h-5 absolute left-3 top-1/2 transform -translate-y-1/2" />
       </div>
       {/* content */}
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
--- src/app/(dashboard)/(routes)/orders/page.tsx
+++ src/app/(dashboard)/(routes)/orders/page.tsx
@@ -1,5 +1,102 @@
-const OrdersPage = () => {
-  return <div>xd</div>;
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { PlusCircle, Search } from "lucide-react";
+import { DataTable } from "./data-table";
+import { columns, Payment } from "./columns";
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ];
+}
+
+const OrdersPage = async () => {
+  const data = await getData();
+  return (
+    <div className="m-4 p-4 space-y-4">
+      {/* header */}
+      <div className="flex justify-between">
+        <div>
+          <h1 className="text-3xl font-semibold">Orders Management</h1>
+          <p className="text-muted-foreground">Manage your rental car orders</p>
+        </div>
+        <div>
+          <Button>
+            Add New Order
+            <PlusCircle className="size-4 ml-1" />
+          </Button>
+        </div>
+      </div>
+      {/* search bar */}
+      <div className="relative">
+        <Input
+          className="max-w-3xl pl-10"
+          placeholder="Search orders by id, car id, or location"
+        />
+        <Search className="w-5 h-5 text-muted-foreground absolute left-3 top-1/2 transform -translate-y-1/2" />
+      </div>
+      {/* content */}
+      <Card>
+        <CardHeader>
+          <h1 className="font-semibold text-2xl">All Orders</h1>
+          <p className="text-muted-foreground">Orders table</p>
+        </CardHeader>
+        <CardContent>
+          <DataTable columns={columns} data={data} />
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default OrdersPage;
